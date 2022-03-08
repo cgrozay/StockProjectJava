37b434e8f37e0973de665c20d50a9bbecbda3e9e
çağrı özay <cgrozay@gmail.com>
2022-03-08 09:01:11 +0300

    first commit1

---
+++ StockProjectJava/src/main/java/backhand/StockProjectJava/StockProjectJavaApplication.java
@@ -0,0 +1,13 @@
+package backhand.StockProjectJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockProjectJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockProjectJavaApplication.class, args);
+	}
+
+}
---
+++ StockProjectJava/src/test/java/backhand/StockProjectJava/StockProjectJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package backhand.StockProjectJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockProjectJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
